1ba2ab78fdf87efde2bb7b7975d0d58347109699
Deondre Mayers <deondre-mayers@hotmail.com>
2022-11-01 14:18:07 -0400

    added new files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+var starter=function(){
+    var email_entry = document.getElementById("email");
+    document.getElementsByClassName("btn")[1].onclick=function(e)
+    {
+         var input = email_entry.value;
+         if (input == "") {
+             document.getElementsByClassName("message")[0].innerHTML = "Please enter a valid email address.";
+         } 
+         else{
+             document.getElementsByClassName("message")[0].innerHTML = "Thank you! Your email address " +input + " has been added to our mailing list!";
+         }
+    }
+}
+ window.onload=starter;
